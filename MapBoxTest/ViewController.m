a8df6716e3f5f964e99779a29a17e8f4316f4fcc
Adrien Long <adrien.long@corellis.eu>
2016-06-30 12:18:52 +0200

    Add some debug in console

--- MapBoxTest/ViewController.m
+++ MapBoxTest/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <MGLMapViewDelegate>
 
 @end
 
@@ -17,14 +17,66 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self.mapView setDelegate:self];
     
     [self.mapView setUserTrackingMode:MGLUserTrackingModeFollowWithHeading];
+    [self displayTrackingModeWithMessage:@"viewDidLoad"];
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+        [self displayTrackingModeWithMessage:@"5 scd before"];
+        
+        [self.mapView setUserTrackingMode:MGLUserTrackingModeFollowWithHeading];
+        
+        [self displayTrackingModeWithMessage:@"5 scd after"];
+        
         [self.mapView setCenterCoordinate:self.mapView.userLocation.coordinate
                                 zoomLevel:15
                                  animated:YES];
     });
+}
+
+- (void)mapViewDidFinishLoadingMap:(MGLMapView *)mapView
+{
+    [self displayTrackingModeWithMessage:@"mapViewDidFinishLoadingMap"];
+}
+
+- (void)mapViewDidFinishRenderingMap:(MGLMapView *)mapView fullyRendered:(BOOL)fullyRendered
+{
+    [self displayTrackingModeWithMessage:@"mapViewDidFinishRenderingMap"];
+}
+
+#pragma mark - Private
+
+- (void)displayTrackingModeWithMessage:(NSString *)message
+{
+    NSString *trackingMode = trackingModeToString(self.mapView.userTrackingMode);
+    
+    if (trackingMode) {
+        NSLog(@"\n\n%@ \n\t:: Tracking Mode :: %@", message, trackingMode);
+    }
+}
+
+NS_INLINE NSString *trackingModeToString(MGLUserTrackingMode mode)
+{
+    switch (mode) {
+        case MGLUserTrackingModeNone: {
+            return @"None";
+        } break;
+        case MGLUserTrackingModeFollow: {
+            return @"Follow";
+        } break;
+        case MGLUserTrackingModeFollowWithCourse: {
+            return @"FollowWithCourse";
+        } break;
+        case MGLUserTrackingModeFollowWithHeading: {
+            return @"FollowWithHeading";
+        } break;
+        default:
+            break;
+    }
+    
+    return @"Unknown";
 }
 
 @end
